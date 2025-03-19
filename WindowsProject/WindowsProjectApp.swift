094447ae5c29f591fcabb0cf03cbcde6dcd5bdd1
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-19 14:55:23 +0530

    Initial Commit

---
+++ WindowsProject/WindowsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowsProjectApp.swift
+//  WindowsProject
+//
+//  Created by Vincent Joy on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WindowsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

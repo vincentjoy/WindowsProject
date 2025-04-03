de50f3ddba1db9c20b8660830d1784cb05ddd711
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-03 23:21:22 +0530

    added 360 degree experiece video

--- WindowsProject/360 Degree Experience/ThreeSixtyDegreeExperience.swift
+++ WindowsProject/360 Degree Experience/ThreeSixtyDegreeExperience.swift
@@ -7,30 +7,53 @@
 
 import SwiftUI
 import RealityKit
+import AVFoundation
 
 struct ThreeSixtyDegreeExperience: View {
     var body: some View {
         RealityView { content in
             // Create a dynamic skybox
+            guard let dynamicSkybox = createDynamicSkybox() else {
+                return
+            }
             
             // Add skybox to contact
+            content.add(dynamicSkybox)
         }
     }
     
     private func createDynamicSkybox() -> Entity? {
         // Mesh
+        let skyboxMesh = MeshResource.generateSphere(radius: 1000)
         
         // Video material
+        guard let videoMaterial = createVideoMaterial() else {
+            return nil
+        }
         
         // Entity
+        let skyboxEntity = ModelEntity(mesh: skyboxMesh, materials: [videoMaterial])
+        
+        // Map to inner surface
+        skyboxEntity.scale *= .init(x: -1, y: 1, z: 1)
+        
+        return skyboxEntity
     }
     
     private func createVideoMaterial() -> VideoMaterial? {
         // Video url
+        guard let url = Bundle.main.url(forResource: "tree", withExtension: "mp4") else {
+            return nil
+        }
         
         // AVPlayer
+        let avPlayer = AVPlayer(url: url)
         
         // Video material
+        let videoMaterial = VideoMaterial(avPlayer: avPlayer)
+        avPlayer.play()
+        
+        return videoMaterial
     }
 }
 
